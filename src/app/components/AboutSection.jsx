577f2be81550362b9be8e2696c1b3f19d2861163
George Yu <george@think.digital>
2025-01-09 21:48:04 +1100

    Add about me section

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,83 @@
+"use client";
+import React, {useTransition, useState} from 'react';
+import Image from 'next/image';
+import TabButton from './TabButton';
+
+const TAB_DATA = [
+    {
+        title: "Skills",
+        id: "skills",
+        content: (
+            <ul className='list-disc pl-2'>
+                <li>Web Development</li>
+                <li>Full Stack Development</li>
+                <li>Database Management</li>
+                <li>Machine Learning</li>
+                <li>Data Analysis</li>
+            </ul>
+        )
+    },
+    {
+        title: "Education",
+        id: "education",
+        content: (
+            <ul className='list-disc pl-2'>
+                <li>Master of Computer Science, University of Adelaide</li>
+                <li>Bachelor of Science, Data Science Major, University of Melbourne</li>
+                <li>Secondary Education, Trinity Grammar School, Kew</li>
+            </ul>
+        )
+    },
+    {
+        title: "Tools",
+        id: "tools",
+        content: (
+            <ul className='list-disc pl-2'>
+                <li>React</li>
+                <li>Node.js</li>
+                <li>Express</li>
+                <li>MySQL</li>
+                <li>mongoDB</li>
+                <li>Python</li>
+                <li>Git</li>
+            </ul>
+        )
+    },
+]
+
+const AboutSection = () => {
+
+    const [tab, setTab] = useState("skills");
+    const [isPending, startTransition] = useTransition();
+
+    const handleTabChange = (id) => {
+        startTransition(() => {
+            setTab(id);
+        })
+    }
+
+    return (
+        <section className='text-white'>
+            <div className='md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
+                <Image src="/images/workspace.png" alt='workspace' width={500} height={500} className="h-96 object-cover"/>
+                <div className='mt-4 md:mt-0 text-left flex flex-col h-full'>
+                    <h2 className='text-4xl font-bold text-white mb-4'>About Me</h2>
+                    <p className='text-base lg:text-lg'>
+                    I am a software engineer with a strong foundation in building robust, scalable systems and applications. My expertise spans full-stack development, including frontend and backend technologies, with a focus on performance, scalability, and user experience. I am dedicated to writing clean, maintainable code and delivering high-quality software solutions that meet business needs and drive innovation.
+                    </p>
+                    <div className="flex flex-row justify-start mt-8">
+                        <TabButton selectTab={() => handleTabChange("skills")} active={tab === "skills"}>{" "}Skills{" "}</TabButton>
+                        <TabButton selectTab={() => handleTabChange("education")} active={tab === "education"}>{" "}Education{" "}</TabButton>
+                        <TabButton selectTab={() => handleTabChange("tools")} active={tab === "tools"}>{" "}Tools{" "}</TabButton>
+                        
+                    </div>
+                    <div className='mt-8'>
+                        {TAB_DATA.find((t) => t.id === tab).content}
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default AboutSection;
--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -13,32 +13,41 @@ const HeroSection = () => {
                 <div className='col-span-7 place-self-center text-center sm:text-left'>
                     <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
                         <span className='text-transparent bg-clip-text bg-gradient-to-r from-teal-500 to-blue-600 '>
-                            Hello, I'm {""} 
+                            Hello World, I'm {""} 
                         </span>
                         <br />
                         <TypeAnimation
                             sequence={[
-                                // Same substring at the start will only be typed out once, initially
                                 'George Yu',
-                                1000, // wait 1s before replacing "Mice" with "Hamsters"
-                                'Web Developer',
-                                1000,
+                                2000, 
+                                'SW Engineer',
+                                2000,
                                 'ML Engineer',
-                                1000,
+                                2000,
                                 'Leetcoder',
-                                1000
+                                2000
                             ]}
                             wrapper="span"
                             speed={50}
                             repeat={Infinity}
                             />
                     </h1>
-                    <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. A deserunt ducimus, nemo labore facere maxime laboriosam ad temporibus accusantium. Voluptatibus assumenda dolorum iure consequuntur consequatur labore commodi velit magnam perspiciatis.
-                    </p>
+                    {/* <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+                        I am a software engineer with a strong foundation in building robust, scalable systems and applications. My expertise spans full-stack development, including frontend and backend technologies, with a focus on performance, scalability, and user experience. I am dedicated to writing clean, maintainable code and delivering high-quality software solutions that meet business needs and drive innovation.
+                    </p> */}
                     <div>
                         <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-teal-500 to-blue-600  text-white font-semibold'>Hire Me</button>
-                        <button className='px-6 py-3 w-full sm:w-fit rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3 font-semibold'>Download CV</button>
+                        <button
+                            className="px-6 py-3 w-full sm:w-fit rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3 font-semibold"
+                            onClick={() => {
+                                const link = document.createElement('a');
+                                link.href = '/files/Resume.pdf';  // path to your CV
+                                link.download = 'Resume_George.pdf';     // desired file name
+                                link.click();                   // programmatically trigger download
+                            }}
+                            >
+                            Resume
+                            </button>
                     </div>
                 </div>
                 <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
